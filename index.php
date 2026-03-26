65759fad2444b8b3d500488ae382ffbd9483472c
Dez <dezuser@gmail.com>
2026-03-26 13:22:00 +0300

    часть 1 2 задание

--- index.php
+++ index.php
@@ -13,4 +13,18 @@ try {
 } catch (Exception $ex) {
     echo 'Исключение: ' . $ex->getMessage();
 }
+$a = 100;
+$b = 0;
+
+try {
+    if ($b == 0) {
+        throw new Exception("Ошибка: деление на ноль!");
+    }
+    $result = $a / $b;
+    echo "Результат: $result";
+} catch (Exception $ex) {
+    $message = date('Y-m-d H:i:s') . " - " . $ex->getMessage() . "\n";
+    file_put_contents('log.txt', $message, FILE_APPEND);
+    echo '<br>Исключение: ' . $ex->getMessage();
+}
 ?>
