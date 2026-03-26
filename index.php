5f546ae8e5a874c14f4c28cf3d078d226514ef03
Dez <dezuser@gmail.com>
2026-03-26 11:44:18 +0300

    часть 1 4 задание

--- index.php
+++ index.php
@@ -10,4 +10,13 @@ while (!feof($file)) {
 fclose($file);
 rename("test.txt", "mir.txt") or die("Ошибка переименования файла");
 echo "Файл переименован в mir.txt<br>";
+// Создаём папку
+if (!file_exists("folder")) {
+    mkdir("folder", 0775, true);
+    echo "Папка 'folder' создана.<br>";
+}
+
+// Перемещаем файл
+rename("mir.txt", "folder/mir.txt") or die("Ошибка перемещения файла");
+echo "Файл mir.txt перемещён в папку folder/<br>";
 ?>
