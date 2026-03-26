bcc09f31ea7d1cd11fafeeb82c5411d470486739
Dez <dezuser@gmail.com>
2026-03-26 11:43:28 +0300

    часть 1 2 задание

--- index.php
+++ index.php
@@ -3,4 +3,11 @@ $file = fopen("test.txt", "w");
 fwrite($file, "Привет, мир!");
 fclose($file);
 echo "Файл test.txt создан и записан.<br>";
+$file = fopen("test.txt", "r");
+while (!feof($file)) {
+    echo fgets($file, 1024) . "<br />";
+}
+fclose($file);
+rename("test.txt", "mir.txt") or die("Ошибка переименования файла");
+echo "Файл переименован в mir.txt<br>";
 ?>
